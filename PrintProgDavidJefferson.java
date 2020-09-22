045ad757c11aff8920cdb013678e72d6348fb706
DavidJefferson1275 <71669271+DavidJefferson1275@users.noreply.github.com>
2020-09-21 20:02:07 -0500

    Add files via upload

---
+++ PrintProgDavidJefferson.java
@@ -0,0 +1,68 @@
+public class PrintProgDavidJefferson
+{
+
+    public static void main(String[] args)
+    {
+       //program1
+
+       System.out.println("     +------+     ");
+       System.out.println("     | DAVE |     ");
+       System.out.println("     +------+     ");
+
+       //program2
+
+       System.out.println();
+       System.out.println("       /////      ");
+       System.out.println("     | O  O |     ");
+       System.out.println("    (|   ^  |)    ");
+       System.out.println("     | [__] |     ");
+       System.out.println("       -----      ");
+
+       //program3
+
+       System.out.println();
+       System.out.println("     +---+---+---+  ");
+       System.out.println("     |   |   |   |  ");
+       System.out.println("     +---+---+---+  ");
+       System.out.println("     +---+---+---+  ");
+       System.out.println("     |   |   |   |  ");
+       System.out.println("     +---+---+---+  ");
+       System.out.println("     +---+---+---+  ");
+       System.out.println("     |   |   |   |  ");
+       System.out.println("     +---+---+---+  ");
+
+       //program4
+
+       System.out.println();
+       System.out.println("             +---+  ");
+       System.out.println("             |   |  ");
+       System.out.println("             +---+  ");
+       System.out.println("         +---+---+  ");
+       System.out.println("         |   |   |  ");
+       System.out.println("         +---+---+  ");
+       System.out.println("     +---+---+---+  ");
+       System.out.println("     |   |   |   |  ");
+       System.out.println("     +---+---+---+  ");
+       System.out.println(" +---+---+---+---+  ");
+       System.out.println(" |   |   |   |   |  ");
+       System.out.println(" +---+---+---+---+  ");
+
+//program5
+
+       System.out.println();
+       System.out.println("             |---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---|           ");
+       System.out.println("             |   |                    |              |     __________________    |           ");
+       System.out.println("             |   |                    |              |     |                     |           ");
+       System.out.println("             +   |                    |              |     |                     +           ");
+       System.out.println("             |   |                    |              |     |                     |           ");
+       System.out.println("             |   |                    |              |     |                     |           ");
+       System.out.println("             |   |                    |______________|     |________________     |           ");
+       System.out.println("             +   |                    |              |                      |    +           ");
+       System.out.println("             |   |                    |              |                      |    |           ");
+       System.out.println("             |   |                    |              |                      |    |           ");
+       System.out.println("             |   |                    |              |                      |    |           ");
+       System.out.println("             +   |_______________     |              |      ________________|    +           ");
+
+    }
+
+}
